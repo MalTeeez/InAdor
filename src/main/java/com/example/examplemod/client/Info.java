5462c4bd7b406ca42e713e3aedac8d1f9aa6ce29
bfu4 <46942366+bfu4@users.noreply.github.com>
2020-09-17 13:08:55 -0400

    info commenting

---
+++ src/main/java/com/example/examplemod/client/Info.java
@@ -0,0 +1,10 @@
+package com.example.examplemod.client;
+
+public class Info {
+    /*
+    todo
+     this package is for everything that is ONLY
+      emphasis on ONLY on the client side. guis/renderers/etc.
+       https://mcforge.readthedocs.io/en/latest/gettingstarted/structuring/
+     */
+}
---
+++ src/main/java/com/example/examplemod/util/Registry.java
@@ -0,0 +1,13 @@
+package com.example.examplemod.util;
+
+public class Registry {
+
+    /*
+     todo
+      this is most likely the registry class
+        which will register blocks / items / etc
+          unless more are needed, a possible RegistryUtil
+            feel free to refactor every file i put in here, these are
+              only for informational purposes
+     */
+}
