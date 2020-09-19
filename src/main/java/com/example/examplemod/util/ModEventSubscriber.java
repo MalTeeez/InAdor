e75d335a0badbcabc9c8f0d4680f0d2fff68617c
MalTeeez <mikahensel@gmx.de>
2020-09-19 17:58:30 +0200

    Block Textures! and another shiro move <3

--- src/main/java/com/example/examplemod/util/ModEventSubscriber.java
+++ src/main/java/com/example/examplemod/util/ModEventSubscriber.java
@@ -22,27 +22,6 @@ public class ModEventSubscriber {
                 setup(new Item(new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP)), "hello_inventory"),
                 setup(new ItemShiro(TierShiro.shiro, 120, 9.0F, ItemShiro.properties.group(ModItemGroups.MOD_ITEM_GROUP)), "shiro")
         );
-       /*
-       Sad try to get BlockItems :(
-        final IForgeRegistry<Item> registry = event.getRegistry();
-        // Automatically register BlockItems for all our Blocks
-        ModBlocks.BLOCKS.getEntries().stream()
-                .map(RegistryObject::get)
-                // You can do extra filtering here if you don't want some blocks to have an BlockItem automatically registered for them
-                // .filter(block -> needsItemBlock(block))
-                // Register the BlockItem for the block
-                .forEach(block -> {
-                    // Make the properties, and make it so that the item will be on our ItemGroup (CreativeTab)
-                    final Item.Properties properties = new Item.Properties().group(ModItemGroups.MOD_ITEM_GROUP);
-                    // Create the new BlockItem with the block and it's properties
-                    final BlockItem blockItem = new BlockItem(block, properties);
-                    // Set the new BlockItem's registry name to the block's registry name
-                    blockItem.setRegistryName(block.getRegistryName());
-                    // Register the BlockItem
-                    registry.register(blockItem);
-                });
-        LOGGER.debug("Registered BlockItems");
-        */
     }
 
     @SubscribeEvent
