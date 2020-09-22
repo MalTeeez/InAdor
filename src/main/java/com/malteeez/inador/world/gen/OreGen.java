df118556f2558233b8de5fad805b936104efb08a
MalTeeez <mikahensel@gmx.de>
2020-09-22 06:22:43 +0200

    no message

--- src/main/java/com/malteeez/inador/world/gen/OreGen.java
+++ src/main/java/com/malteeez/inador/world/gen/OreGen.java
@@ -1,7 +1,6 @@
 package com.malteeez.inador.world.gen;
 
 
-import com.ibm.icu.impl.duration.impl.DataRecord;
 import com.malteeez.inador.InAdor;
 import com.malteeez.inador.util.BlockRegister;
 import net.minecraft.block.BlockState;
@@ -10,7 +9,6 @@ import net.minecraft.block.pattern.BlockMatcher;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.OreFeature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.placement.ConfiguredPlacement;
 import net.minecraft.world.gen.placement.CountRangeConfig;
@@ -23,7 +21,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = InAdor.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class OreGen {
-
 
     public static OreFeatureConfig.FillerBlockType END_STONE = OreFeatureConfig.FillerBlockType.create("END_STONE","end_stone", new BlockMatcher(Blocks.END_STONE));
 
@@ -40,7 +37,6 @@ public class OreGen {
             }
         }
     }
-
     /**
      * @param biome //This should be self explanatory
      * @param count //rareness multiplier = 1 lowest
@@ -51,7 +47,6 @@ public class OreGen {
      * @param defaultBlockState //The Block to generate
      * @param size //max Vein size
      */
-
     private static void genOre(Biome biome, int count, int bottomOffset, int topOffset, int max, OreFeatureConfig.FillerBlockType filler, BlockState defaultBlockState, int size) {
         CountRangeConfig range = new CountRangeConfig(count, bottomOffset, topOffset, max);
         OreFeatureConfig feature = new OreFeatureConfig(filler, defaultBlockState, size);
