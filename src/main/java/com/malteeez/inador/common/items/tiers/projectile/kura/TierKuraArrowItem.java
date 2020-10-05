bb49b49e0ea93dab48103adf5e8f7069f14cfa3a
bfu4 <bellafusari@vivaldi.net>
2020-10-05 12:38:44 -0400

    Fixed bow NPE

--- src/main/java/com/malteeez/inador/common/items/tiers/projectile/kura/TierKuraArrowItem.java
+++ src/main/java/com/malteeez/inador/common/items/tiers/projectile/kura/TierKuraArrowItem.java
@@ -1,6 +1,12 @@
 package com.malteeez.inador.common.items.tiers.projectile.kura;
 
+import com.malteeez.inador.common.entity.TierKuraArrowEntity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.projectile.AbstractArrowEntity;
+import net.minecraft.entity.projectile.ArrowEntity;
 import net.minecraft.item.ArrowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class TierKuraArrowItem extends ArrowItem {
 
@@ -8,4 +14,10 @@ public class TierKuraArrowItem extends ArrowItem {
         super(builder);
     }
 
+    @Override
+    public TierKuraArrowEntity createArrow(World worldIn, ItemStack stack, LivingEntity shooter) {
+        TierKuraArrowEntity arrowentity = new TierKuraArrowEntity(worldIn, shooter);
+        arrowentity.setPotionEffect(stack);
+        return arrowentity;
+    }
 }
